2f04f3ad4e37e3db840d08d6670cdf7b9837652a
pajakwsei <mpajak@wsei.edu.pl>
2021-11-27 17:25:05 +0100

    Added New and Edit forms for the Books model

--- Models/Book.cs
+++ Models/Book.cs
@@ -16,9 +16,13 @@ namespace LibApp.Models
 		public string AuthorName { get; set; }
 		[Required]
 		public Genre Genre { get; set; }
+		[Required]
+		[Display(Name="Genre")]
 		public byte GenreId { get; set; }
 		public DateTime DateAdded { get; set; }
+		[Display(Name="Release Date")]
 		public DateTime ReleaseDate { get; set; }
+		[Display(Name = "Number in Stock")]
 		public int NumberInStock { get; set; }
 	}
       
