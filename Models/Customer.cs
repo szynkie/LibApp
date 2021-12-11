a3849639028f22d48c7d178ba585df755a26e91e
pajakwsei <mpajak@wsei.edu.pl>
2021-12-11 16:43:41 +0100

    Added server-side validation for CustomerForm

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -6,14 +6,16 @@ namespace LibApp.Models
     public class Customer
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please provide customer's name")]
         [StringLength(255)]
         public string Name { get; set; }
         public bool HasNewsletterSubscribed { get; set; }
         public MembershipType MembershipType { get; set; }
         [Display(Name="Membership Type")]
+        [Required(ErrorMessage = "Please provide Membership Type")]
         public byte MembershipTypeId { get; set; }
         [Display(Name="Date of Birth")]
+        [Min18YearsIfMember]
         public DateTime? Birthdate { get; set; }
     }
 }
