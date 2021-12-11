b0e30de781c4d0f34fedc9b43cf37f1cf5484145
pajakwsei <mpajak@wsei.edu.pl>
2021-12-11 14:02:12 +0100

    Added AutoMapper to application

---
+++ Dtos/CustomerDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibApp.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool HasNewsletterSubscribed { get; set; }
+        [Display(Name = "Membership Type")]
+        public byte? MembershipTypeId { get; set; }
+        [Display(Name = "Date of Birth")]
+        public DateTime? Birthdate { get; set; }
+    }
+}
