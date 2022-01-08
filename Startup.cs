850c21ab9e759a1b8c8e2a1efeca0386e5e50c41
padzak <mrcn.pajak@gmail.com>
2022-01-08 11:28:05 +0100

    Added XML Serializer output formatter

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using LibApp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -31,7 +32,13 @@ namespace LibApp
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
+            
+            //services.AddControllersWithViews();
+
+            services.AddControllersWithViews(options => {
+                options.OutputFormatters.RemoveType(typeof(SystemTextJsonOutputFormatter));
+                options.ReturnHttpNotAcceptable = true;
+            }).AddXmlSerializerFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
