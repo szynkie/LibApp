10c7eed7071fca46c23f4438c3055da186a87fba
padzak <mrcn.pajak@gmail.com>
2022-01-08 13:40:13 +0100

    Added Bootbox.js and DataTables to Customers Index view

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -21,11 +21,7 @@ namespace LibApp.Controllers
 
         public ViewResult Index()
         {
-            var customers = _context.Customers
-                .Include(c => c.MembershipType)
-                .ToList();
-            
-            return View(customers);
+            return View();
         }
 
         public IActionResult Details(int id)
