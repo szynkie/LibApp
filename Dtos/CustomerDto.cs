5cbb42c75e040665e144dc3eb3de391bbc16b181
padzak <mrcn.pajak@gmail.com>
2022-01-08 12:42:58 +0100

    Made records to disappear from table instantly after pushing delete button

--- Dtos/CustomerDto.cs
+++ Dtos/CustomerDto.cs
@@ -15,6 +15,7 @@ namespace LibApp.Dtos
         public string Name { get; set; }
         public bool HasNewsletterSubscribed { get; set; }
         public byte MembershipTypeId { get; set; }
+        public MembershipTypeDto MembershipType { get; set; }
         public DateTime? Birthdate { get; set; }
     }
 }
