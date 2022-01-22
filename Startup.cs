6f6d288cb16cdcddeb7f8d4aecd0a1f6b5b2015b
padzak <mrcn.pajak@gmail.com>
2022-01-22 11:48:18 +0100

    Enabled server session in project

--- Startup.cs
+++ Startup.cs
@@ -35,15 +35,20 @@ namespace LibApp
 
             services.AddControllersWithViews();
 
-            //services.AddControllersWithViews(options => {
-            //    options.OutputFormatters.RemoveType(typeof(SystemTextJsonOutputFormatter));
-            //    options.ReturnHttpNotAcceptable = true;
-            //}).AddXmlSerializerFormatters();
+            services.AddDistributedMemoryCache();
+            services.AddSession(options => {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseSession(); 
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
