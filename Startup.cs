b31fe1ff3c49f206eec0c66ebb4eef7985e9b0c5
padzak <mrcn.pajak@gmail.com>
2022-01-22 11:48:37 +0100

    Removed server session

--- Startup.cs
+++ Startup.cs
@@ -34,21 +34,11 @@ namespace LibApp
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddControllersWithViews();
-
-            services.AddDistributedMemoryCache();
-            services.AddSession(options => {
-                options.IdleTimeout = TimeSpan.FromSeconds(10);
-                options.Cookie.HttpOnly = true;
-                options.Cookie.IsEssential = true;
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
-            app.UseSession(); 
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
