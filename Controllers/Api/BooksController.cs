302af32b59d7b9bbf64c2e844385d4f0ad93c853
padzak <mrcn.pajak@gmail.com>
2022-01-22 15:40:23 +0100

    Added API controller for Book and DTO

---
+++ Controllers/Api/BooksController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using LibApp.Data;
+using LibApp.Dtos;
+using LibApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibApp.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public BooksController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public IEnumerable<BookDto> GetBooks(string query = null)
+        {
+            var booksQuery = _context.Books.Where(b => b.NumberAvailable > 0);
+
+            if (!String.IsNullOrWhiteSpace(query))
+            {
+                booksQuery = booksQuery.Where(b => b.Name.Contains(query));
+            }
+
+            return booksQuery.ToList().Select(_mapper.Map<Book, BookDto>);
+        }
+
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+    }
+}
